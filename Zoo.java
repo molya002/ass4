d907ab2df00b84ddd5d32b57b536eba4e9e3b2bf
molya002 <moldir000.29e@gmail.com>
2026-01-26 04:36:06 +0500

    Add files via upload

---
+++ Zoo.java
@@ -0,0 +1,15 @@
+public class Zoo {
+    private String name;
+    private String location;
+    private int size;
+
+    public Zoo(String name, String location, int size) {
+        this.name = name;
+        this.location = location;
+        this.size = size;
+    }
+
+    public String getName() { return name; }
+    public String getLocation() { return location; }
+    public int getSize() { return size; }
+}
