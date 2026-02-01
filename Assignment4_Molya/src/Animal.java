03f1ae36b382c7dfa4a08ae4b3f5bde3c7c489e6
molya002 <moldir000.29e@gmail.com>
2026-02-01 22:04:32 +0300

    Add files via upload

---
+++ Assignment4_Molya/src/Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+    private String name;
+    private String type;
+    private int zooId;
+
+    public Animal(String name, String type, int zooId) {
+        this.name = name;
+        this.type = type;
+        this.zooId = zooId;
+    }
+
+    public String getName() { return name; }
+    public String getType() { return type; }
+    public int getZooId() { return zooId; }
+}
---
+++ Assignment4_Molya/src/DBConnection.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    private static final String URL =
+            "jdbc:postgresql://localhost:5432/molya_Assigment4";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "molya12341234";
+
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            throw new RuntimeException("Database connection failed", e);
+        }
+    }
+}
